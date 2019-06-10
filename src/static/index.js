10028f97d5f9ec113f43b4af763bfd23b46c567a
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-06-10 11:53:04 +0900

    Chat with SocketIO

--- src/static/index.js
+++ src/static/index.js
@@ -2,4 +2,18 @@ const socket = io("/");
 
 socket.on("hello", () => console.log("Somebody joined"));
 
-setTimeout(() => socket.emit("helloGuys"), 4000);
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`You: ${message}`);
+}
+
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
