542e6a0eafa0e05e47ab09dce32d2bb9a2037580
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-06-19 14:24:03 +0900

    notification fix

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -4,13 +4,12 @@ const fireNotification = (text, color) => {
   const notification = document.createElement("div");
   notification.innerText = text;
   notification.style.backgroundColor = color;
+  notification.className = "notification";
   notifications.appendChild(notification);
 };
 
-export const handleNewUser = ({ nickname }) => {
+export const handleNewUser = ({ nickname }) =>
   fireNotification(`${nickname} just joined!`, "rgb(0, 122, 255)");
-};
 
-export const handleDisconnected = ({ nickname }) => {
+export const handleDisconnected = ({ nickname }) =>
   fireNotification(`${nickname} just left!`, "rgb(255, 149, 0)");
-};
